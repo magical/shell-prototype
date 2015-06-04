36c09295098245b2580f5cc03823f316bec4bbbc
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2015-06-03 20:33:14 -0700

    Cursor

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <X11/Xlib.h>
+#include <X11/keysym.h>
 #include <cairo/cairo-xlib.h>
 #include <pango/pangocairo.h>
 
@@ -46,19 +47,33 @@ cairo_surface_t *cairo_create_x11_surface(Display *display, int x, int y) {
     return surface;
 }
 
+int cursor_pos = 1;
+
 void draw_text(cairo_t *cr, PangoLayout *layout, const char* text) {
     cairo_move_to(cr, 10, 20);
     cairo_set_source_rgb(cr, 0, 0, 1);
     pango_layout_set_text(layout, text, -1);
+    pango_layout_set_wrap(layout, PANGO_WRAP_WORD_CHAR);
     pango_cairo_update_layout(cr, layout);
     pango_cairo_show_layout(cr, layout);
+
+    // Cursor
+    PangoRectangle rect;
+    pango_layout_index_to_pos(layout, cursor_pos, &rect);
+    rect.x += 10*PANGO_SCALE;
+    rect.y += 20*PANGO_SCALE;
+    double d = PANGO_SCALE;
+    cairo_rectangle(cr, rect.x/d - 1, rect.y/d, 2, rect.height/d);
+    cairo_clip(cr);
+    cairo_set_source_rgb(cr, 0, 0, 0);
+    cairo_paint(cr);
 }
 
 void redraw(cairo_t *cr, PangoLayout *layout) {
     cairo_push_group(cr);
     cairo_set_source_rgb(cr, 1, 1, 1);
     cairo_paint(cr);
-    draw_text(cr, layout, "Hello, world!");
+    draw_text(cr, layout, "Hello, world! abdfgylqptjkb");
     cairo_pop_group_to_source(cr);
     cairo_paint(cr);
 }
@@ -68,18 +83,42 @@ int event_loop(Display *display, cairo_surface_t *surface) {
     cairo_t *cr = cairo_create(surface);
     PangoLayout *layout = pango_cairo_create_layout(cr);
 
-    PangoFontDescription *desc = pango_font_description_from_string("Dina 10");
+    PangoFontDescription *desc = pango_font_description_from_string("Sans 10");
     pango_layout_set_font_description(layout, desc);
     pango_font_description_free(desc);
 
+    KeySym sym;
     int index;
     int trailing;
     for (;;) {
         XNextEvent(display, &e);
         switch (e.type) {
         case ButtonPress:
-            pango_layout_xy_to_index(layout, e.xbutton.x, e.xbutton.y, &index, &trailing);
+            pango_layout_xy_to_index(layout,
+                (e.xbutton.x-10)*PANGO_SCALE,
+                (e.xbutton.y-20)*PANGO_SCALE,
+                &index, &trailing);
             printf("%d,%d = %d (%d)\n", e.xbutton.x, e.xbutton.y, index, trailing);
+            cursor_pos = index;
+            goto case_expose;
+
+        case KeyPress:
+            sym = XLookupKeysym(&e.xkey, 0);
+            switch(sym) {
+            case XK_Left:
+                cursor_pos -= 1;
+                goto case_expose;
+            case XK_Right:
+                cursor_pos += 1;
+                goto case_expose;
+            case XK_q:
+                goto cleanup;
+            default:
+                printf("unknown key %ld\n", sym);
+            }
+            break;
+
+        case KeyRelease:
             break;
 
         case ClientMessage:
@@ -98,6 +137,7 @@ int event_loop(Display *display, cairo_surface_t *surface) {
             break;
 
         case Expose:
+        case_expose:
             //fprintf(stderr, "got exposure event\n");
             redraw(cr, layout);
             cairo_surface_flush(surface);
