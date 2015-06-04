67b8de82f6bad39c3c64022b6eab575830adc32a
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2015-06-03 22:35:43 -0700

    Term object.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
@@ -8,6 +9,21 @@
 
 Atom wm_protocols;
 Atom wm_delete_window;
+
+typedef struct Term Term;
+struct Term {
+    Display *display;
+    cairo_surface_t *surface;
+    cairo_t *cr;
+    PangoLayout *layout;
+
+    char *text;
+    int textlen;
+    int cursor_pos;
+    int cursor_type;
+
+    char *scrollback;
+};
 
 cairo_surface_t *cairo_create_x11_surface(Display *display, int x, int y) {
     int screen;
@@ -47,8 +63,6 @@ cairo_surface_t *cairo_create_x11_surface(Display *display, int x, int y) {
     return surface;
 }
 
-int cursor_pos = 1;
-
 void draw_text(cairo_t *cr, PangoLayout *layout, const char* text) {
     cairo_move_to(cr, 10, 20);
     cairo_set_source_rgb(cr, 0, 0, 1);
@@ -56,60 +70,75 @@ void draw_text(cairo_t *cr, PangoLayout *layout, const char* text) {
     pango_layout_set_wrap(layout, PANGO_WRAP_WORD_CHAR);
     pango_cairo_update_layout(cr, layout);
     pango_cairo_show_layout(cr, layout);
-
-    // Cursor
-    PangoRectangle rect;
-    pango_layout_index_to_pos(layout, cursor_pos, &rect);
-    rect.x += 10*PANGO_SCALE;
-    rect.y += 20*PANGO_SCALE;
-    double d = PANGO_SCALE;
-    cairo_rectangle(cr, rect.x/d - 1, rect.y/d, 2, rect.height/d);
-    cairo_clip(cr);
-    cairo_set_source_rgb(cr, 0, 0, 0);
-    cairo_paint(cr);
 }
 
-void redraw(cairo_t *cr, PangoLayout *layout) {
+void draw_cursor(Term *t) {
+    PangoRectangle rect;
+    pango_layout_index_to_pos(t->layout, t->cursor_pos, &rect);
+    double d = PANGO_SCALE;
+    rect.x += 10*d;
+    rect.y += 20*d;
+    cairo_set_source_rgb(t->cr, 0, 0, 0);
+    switch (t->cursor_type) {
+    default:
+        cairo_rectangle(t->cr, rect.x/d, rect.y/d, rect.width/d, rect.height/d);
+        break;
+    case 1:
+        cairo_rectangle(t->cr, rect.x/d+.5, rect.y/d+.5, rect.width/d-1, rect.height/d-1);
+        cairo_set_line_width(t->cr, 1);
+        cairo_stroke(t->cr);
+        break;
+    case 2:
+        cairo_rectangle(t->cr, rect.x/d - 1, rect.y/d, 1, rect.height/d);
+        cairo_rectangle(t->cr, rect.x/d - 2, rect.y/d - 2, 3, 3);
+        cairo_rectangle(t->cr, rect.x/d - 2, (rect.y + rect.height)/d - 2, 3, 3);
+        break;
+    }
+    cairo_clip(t->cr);
+    cairo_paint(t->cr);
+}
+
+void redraw(Term *t) {
+    cairo_t *cr = t->cr;
     cairo_push_group(cr);
     cairo_set_source_rgb(cr, 1, 1, 1);
     cairo_paint(cr);
-    draw_text(cr, layout, "Hello, world! abdfgylqptjkb");
+    draw_text(cr, t->layout, "Hello, world! abdfgylqptjkb");
+    draw_cursor(t);
     cairo_pop_group_to_source(cr);
     cairo_paint(cr);
+    cairo_surface_flush(t->surface);
 }
 
-int event_loop(Display *display, cairo_surface_t *surface) {
+int event_loop(Term *t) {
     XEvent e;
-    cairo_t *cr = cairo_create(surface);
-    PangoLayout *layout = pango_cairo_create_layout(cr);
-
-    PangoFontDescription *desc = pango_font_description_from_string("Sans 10");
-    pango_layout_set_font_description(layout, desc);
-    pango_font_description_free(desc);
-
     KeySym sym;
     int index;
     int trailing;
     for (;;) {
-        XNextEvent(display, &e);
+        XNextEvent(t->display, &e);
         switch (e.type) {
         case ButtonPress:
-            pango_layout_xy_to_index(layout,
+            pango_layout_xy_to_index(t->layout,
                 (e.xbutton.x-10)*PANGO_SCALE,
                 (e.xbutton.y-20)*PANGO_SCALE,
                 &index, &trailing);
             printf("%d,%d = %d (%d)\n", e.xbutton.x, e.xbutton.y, index, trailing);
-            cursor_pos = index;
+            t->cursor_pos = index;
             goto case_expose;
 
         case KeyPress:
             sym = XLookupKeysym(&e.xkey, 0);
             switch(sym) {
             case XK_Left:
-                cursor_pos -= 1;
+                if (t->cursor_pos - 1 >= 0) {
+                    t->cursor_pos -= 1;
+                }
                 goto case_expose;
             case XK_Right:
-                cursor_pos += 1;
+                if (t->cursor_pos + 1 <= t->textlen) {
+                    t->cursor_pos += 1;
+                }
                 goto case_expose;
             case XK_q:
                 goto cleanup;
@@ -130,17 +159,16 @@ int event_loop(Display *display, cairo_surface_t *surface) {
 
         case ConfigureNotify:
             //fprintf(stderr, "got configure event\n");
-            cairo_xlib_surface_set_size(surface,
+            cairo_xlib_surface_set_size(t->surface,
                 e.xconfigure.width, e.xconfigure.height);
-            pango_layout_set_width(layout,
+            pango_layout_set_width(t->layout,
                 (e.xconfigure.width - 20)*PANGO_SCALE);
             break;
 
         case Expose:
         case_expose:
             //fprintf(stderr, "got exposure event\n");
-            redraw(cr, layout);
-            cairo_surface_flush(surface);
+            redraw(t);
             break;
 
         default:
@@ -149,28 +177,48 @@ int event_loop(Display *display, cairo_surface_t *surface) {
     }
 
 cleanup:
-    g_object_unref(layout);
-    cairo_destroy(cr);
     return 0;
 }
 
 int main() {
-    Display *display;
-    cairo_surface_t *surface;
+    PangoFontDescription *desc;
+    Term t;
 
-    display = XOpenDisplay(NULL);
-    if (display == NULL) {
+    t.display = XOpenDisplay(NULL);
+    if (t.display == NULL) {
         exit(1);
     }
 
-    wm_protocols = XInternAtom(display, "WM_PROTOCOLS", 0);
-    wm_delete_window = XInternAtom(display, "WM_DELETE_WINDOW", 0);
+    wm_protocols = XInternAtom(t.display, "WM_PROTOCOLS", 0);
+    wm_delete_window = XInternAtom(t.display, "WM_DELETE_WINDOW", 0);
 
-    surface = cairo_create_x11_surface(display, 300, 100);
+    t.surface = cairo_create_x11_surface(t.display, 300, 100);
+    if (t.surface == NULL) {
+        exit(1);
+    }
+    t.cr = cairo_create(t.surface);
+    if (t.cr == NULL) {
+        exit(1);
+    }
+    t.layout = pango_cairo_create_layout(t.cr);
+    if (t.layout == NULL) {
+        exit(1);
+    }
 
-    event_loop(display, surface);
+    desc = pango_font_description_from_string("Dina 10");
+    pango_layout_set_font_description(t.layout, desc);
+    pango_font_description_free(desc);
 
-    cairo_surface_destroy(surface);
-    XCloseDisplay(display);
+    t.text = "Hello, world! abdfgylqptjkb";
+    t.textlen = strlen(t.text);
+    t.cursor_pos = 0;
+    t.cursor_type = 0;
+
+    event_loop(&t);
+
+    g_object_unref(t.layout);
+    cairo_destroy(t.cr);
+    cairo_surface_destroy(t.surface);
+    XCloseDisplay(t.display);
     return 0;
 }
