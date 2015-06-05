ff905d343345b547d151f242ab54aa83473262e5
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2015-06-04 18:53:20 -0700

    Editing.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
+#include <X11/Xutil.h>
 #include <cairo/cairo-xlib.h>
 #include <pango/pangocairo.h>
 
@@ -19,8 +20,11 @@ struct Term {
 
     char *text;
     int textlen;
+    int textcap;
+
     int cursor_pos;
     int cursor_type;
+    int border;
 
     char *scrollback;
 };
@@ -51,7 +55,7 @@ cairo_surface_t *cairo_create_x11_surface(Display *display, int x, int y) {
     XSetWMProtocols(display, win, &wm_delete_window, 1);
 
     // Set window title
-    XStoreName(display, win, "pango cairo");
+    XStoreName(display, win, "magicalterm");
 
     // Show the window and create a cairo surface
     XMapWindow(display, win);
@@ -63,10 +67,9 @@ cairo_surface_t *cairo_create_x11_surface(Display *display, int x, int y) {
     return surface;
 }
 
-void draw_text(cairo_t *cr, PangoLayout *layout, const char* text) {
-    cairo_move_to(cr, 10, 20);
+void draw_text(cairo_t *cr, PangoLayout *layout, const char* text, size_t len) {
     cairo_set_source_rgb(cr, 0, 0, 1);
-    pango_layout_set_text(layout, text, -1);
+    pango_layout_set_text(layout, text, len);
     pango_layout_set_wrap(layout, PANGO_WRAP_WORD_CHAR);
     pango_cairo_update_layout(cr, layout);
     pango_cairo_show_layout(cr, layout);
@@ -76,8 +79,8 @@ void draw_cursor(Term *t) {
     PangoRectangle rect;
     pango_layout_index_to_pos(t->layout, t->cursor_pos, &rect);
     double d = PANGO_SCALE;
-    rect.x += 10*d;
-    rect.y += 20*d;
+    rect.x += t->border*d;
+    rect.y += t->border*d;
     cairo_set_source_rgb(t->cr, 0, 0, 0);
     switch (t->cursor_type) {
     default:
@@ -98,21 +101,31 @@ void draw_cursor(Term *t) {
     cairo_paint(t->cr);
 }
 
-void redraw(Term *t) {
+void term_redraw(Term *t) {
     cairo_t *cr = t->cr;
     cairo_push_group(cr);
     cairo_set_source_rgb(cr, 1, 1, 1);
     cairo_paint(cr);
-    draw_text(cr, t->layout, "Hello, world! abdfgylqptjkb");
+    cairo_move_to(cr, t->border, t->border);
+    draw_text(cr, t->layout, t->text, t->textlen);
     draw_cursor(t);
     cairo_pop_group_to_source(cr);
     cairo_paint(cr);
     cairo_surface_flush(t->surface);
 }
 
+void term_movecursor(Term *t, int n) {
+    if (t->cursor_pos + n >= 0)
+    if (t->cursor_pos + n <= t->textlen) {
+        t->cursor_pos += n;
+    }
+}
+
 int event_loop(Term *t) {
     XEvent e;
     KeySym sym;
+    char buf[32];
+    int n;
     int index;
     int trailing;
     for (;;) {
@@ -120,29 +133,56 @@ int event_loop(Term *t) {
         switch (e.type) {
         case ButtonPress:
             pango_layout_xy_to_index(t->layout,
-                (e.xbutton.x-10)*PANGO_SCALE,
-                (e.xbutton.y-20)*PANGO_SCALE,
+                (e.xbutton.x-t->border)*PANGO_SCALE,
+                (e.xbutton.y-t->border)*PANGO_SCALE,
                 &index, &trailing);
             printf("%d,%d = %d (%d)\n", e.xbutton.x, e.xbutton.y, index, trailing);
             t->cursor_pos = index;
-            goto case_expose;
+            term_redraw(t);
+            break;
 
         case KeyPress:
             sym = XLookupKeysym(&e.xkey, 0);
             switch(sym) {
-            case XK_Left:
-                if (t->cursor_pos - 1 >= 0) {
-                    t->cursor_pos -= 1;
-                }
-                goto case_expose;
-            case XK_Right:
-                if (t->cursor_pos + 1 <= t->textlen) {
-                    t->cursor_pos += 1;
-                }
-                goto case_expose;
-            case XK_q:
+            case XK_Escape:
                 goto cleanup;
+            case XK_Left:
+                term_movecursor(t, -1);
+                term_redraw(t);
+                break;
+            case XK_Right:
+                term_movecursor(t, 1);
+                term_redraw(t);
+                break;
+            case XK_Home:
+                t->cursor_pos = 0;
+                term_redraw(t);
+                break;
+            case XK_End:
+                t->cursor_pos = t->textlen;
+                term_redraw(t);
+                break;
+            case XK_BackSpace:
+                if (t->cursor_pos == t->textlen) {
+                    t->textlen--;
+                }
+                term_movecursor(t, -1);
+                term_redraw(t);
+                break;
             default:
+                // TODO Xmb
+                n = XLookupString(&e.xkey, buf, sizeof buf, &sym, NULL);
+                if (n == 1 && 0x20 <= buf[0] && buf[0] <= 0x7e) {
+                    if (t->cursor_pos < t->textcap) {
+                        if (t->cursor_pos == t->textlen) {
+                            t->textlen++;
+                        }
+                        t->text[t->cursor_pos] = buf[0];
+                        t->cursor_pos += n;
+                    }
+                    term_redraw(t);
+                    break;
+                }
                 printf("unknown key %ld\n", sym);
             }
             break;
@@ -162,13 +202,12 @@ int event_loop(Term *t) {
             cairo_xlib_surface_set_size(t->surface,
                 e.xconfigure.width, e.xconfigure.height);
             pango_layout_set_width(t->layout,
-                (e.xconfigure.width - 20)*PANGO_SCALE);
+                (e.xconfigure.width - 2*t->border)*PANGO_SCALE);
             break;
 
         case Expose:
-        case_expose:
             //fprintf(stderr, "got exposure event\n");
-            redraw(t);
+            term_redraw(t);
             break;
 
         default:
@@ -205,14 +244,17 @@ int main() {
         exit(1);
     }
 
-    desc = pango_font_description_from_string("Dina 10");
+    desc = pango_font_description_from_string("Sans 10");
     pango_layout_set_font_description(t.layout, desc);
     pango_font_description_free(desc);
 
-    t.text = "Hello, world! abdfgylqptjkb";
+    char text[256] = "Hello, world! abdfgylqptjkb";
+    t.text = text;
     t.textlen = strlen(t.text);
+    t.textcap = sizeof text;
     t.cursor_pos = 0;
-    t.cursor_type = 0;
+    t.cursor_type = 1;
+    t.border = 2;
 
     event_loop(&t);
 
